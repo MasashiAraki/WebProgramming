939f25217710f2835fc9464720e53b339233252f
ArakiMasashi <masa121codbf@gmail.com>
2018-06-01 16:25:08 +0900

    ログインIDとパスワードに該当するIDと名前を取得するメソッドを実装

--- Project/src/dao/UserDao.java
+++ Project/src/dao/UserDao.java
@@ -1,5 +1,12 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.apache.catalina.User;
+
 /**
  * UserテーブルのDao
  * @author 31510007-PC
@@ -8,4 +15,43 @@ package dao;
 
 public class UserDao {
 
+	/**
+	 * ログインIDとパスワードに該当するIDと名前を取得
+	 * @param loginId
+	 * @param password
+	 * @return
+	 */
+	public User findByLoginInfo(String loginId, String password) {
+		Connection con = null;
+		try {
+			// DBに接続してSQL文を実行
+			con = DBManager.getConnection();
+			String sql = "SELECT * FROM user WHERE login_id = ? and password = ?";
+			PreparedStatement pStmt = con.prepareStatement(sql);
+			pStmt.setString(1, loginId);
+			pStmt.setString(2, password);
+			ResultSet rs = pStmt.executeQuery();
+
+			if (!rs.next()) {
+				return null;
+			}
+			// フィールドを取得
+			String loginIdData = rs.getString("login_id");
+			String nameData = rs.getString("name");
+			return new User(loginIdData, nameData);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			if(con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+					return null;
+				}
+			}
+		}
+	}
 }
