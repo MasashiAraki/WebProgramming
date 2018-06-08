8bd77f6f8e13a56f56946030b7d27c33ba9ef989
ArakiMasashi <masa121codbf@gmail.com>
2018-06-08 14:30:02 +0900

    ログインサーブレット完成

--- Project/src/controller/LoginServlet.java
+++ Project/src/controller/LoginServlet.java
@@ -1,11 +1,17 @@
 package controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.UserDao;
+import model.User;
 
 /**
  * Servlet implementation class LoginServlet
@@ -13,7 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,16 +32,45 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// フォワード
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/index.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		// 文字コード指定
+		request.setCharacterEncoding("UTF-8");
+		// リクエストパラメータを取得
+		String loginId = request.getParameter("loginId");
+		String password = request.getParameter("password");
+
+		// リクエストパラメータを引数にして、Daoを実行
+		UserDao userDao = new UserDao();
+		User user = userDao.findByLoginInfo(loginId, password);
+
+
+		/**
+		 * テーブルに該当のデータが見つからなかった場合
+		 */
+		if (user == null) {
+			request.setAttribute("errMsg", "ログインに失敗しました");
+			// ログインjspにフォワード
+			RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/index.jsp");
+			dispatcher.forward(request, response);
+			return;
+		}
+
+		/**
+		 * テーブルに該当のデータが見つかった場合
+		 */
+		HttpSession session = request.getSession();
+		session.setAttribute("userInfo", user);
+
+		// ユーザ一覧のサーブレットにリダイレクト
+		response.sendRedirect("UserListServlet");
 	}
 
 }
