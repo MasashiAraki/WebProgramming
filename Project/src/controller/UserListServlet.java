85d788f3a2c0d15b1c25ab9a3589d662684a5598
ArakiMasashi <masa121codbf@gmail.com>
2018-06-12 01:50:18 +0900

    UserBeansのインポート文を追加。動いた。

--- Project/src/controller/UserListServlet.java
+++ Project/src/controller/UserListServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import dao.UserDao;
+import model.User;
 
 /**
  * Servlet implementation class UserListServlet
