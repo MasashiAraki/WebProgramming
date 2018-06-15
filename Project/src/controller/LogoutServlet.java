76160c3093af80cf6fbe2fa6dba98a9264fcce0b
ArakiMasashi <masa121codbf@gmail.com>
2018-06-16 05:13:40 +0900

    ログアウト処理を追加

--- Project/src/controller/LogoutServlet.java
+++ Project/src/controller/LogoutServlet.java
@@ -2,12 +2,12 @@ package controller;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class LogoutServlet
@@ -28,8 +28,13 @@ public class LogoutServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/index.jsp");
-		dispatcher.forward(request, response);
+		HttpSession session = request.getSession();
+
+		// セッションのユーザ情報を削除
+		session.removeAttribute("userInfo");
+
+		// ログインサーブレットへリダイレクト
+		response.sendRedirect("LoginServlet");
 	}
 
 	/**
