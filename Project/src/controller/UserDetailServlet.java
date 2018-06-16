15064c5e12d14c7549f41c20d5da27ffb1262551
ArakiMasashi <masa121codbf@gmail.com>
2018-06-17 04:59:23 +0900

    ログインセッションのユーザ情報の有無によって分岐

--- Project/src/controller/UserDetailServlet.java
+++ Project/src/controller/UserDetailServlet.java
@@ -1,11 +1,14 @@
 package controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class UserDetailServlet
@@ -13,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/UserDetailServlet")
 public class UserDetailServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -25,10 +28,17 @@ public class UserDetailServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	// ログインセッションのユーザ情報の有無によって分岐
+    	HttpSession session = request.getSession();
+    	Object LoginCheck = session.getAttribute("userInfo");
+    	if (LoginCheck == null) {
+    		response.sendRedirect("LoginServlet");
+    	} else {
+    		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/userDetail.jsp");
+    		dispatcher.forward(request, response);
+    	}
+    }
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
