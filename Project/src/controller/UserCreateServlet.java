96a189d276807e4960f055a791a23bbf185b9bc6
ArakiMasashi <masa121codbf@gmail.com>
2018-06-20 16:41:00 +0900

    add：エラー時の処理

--- Project/src/controller/UserCreateServlet.java
+++ Project/src/controller/UserCreateServlet.java
@@ -1,6 +1,9 @@
 package controller;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,6 +12,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import dao.UserDao;
+import model.User;
 
 /**
  * Servlet implementation class UserCreateServlet
@@ -55,8 +61,57 @@ public class UserCreateServlet extends HttpServlet {
 		String userName = request.getParameter("userName");
 		String birthdate = request.getParameter("birthdate");
 
+		// ログインID比較用
+		UserDao userDao = new UserDao();
+		List<User> userList = userDao.findAll();
+		request.setAttribute("userList", userList);
 
 
+		// 登録エラー時に使う
+		Map<String, String> inputParameterMap = new HashMap<>();
+		inputParameterMap.put("loginId", loginId);
+		inputParameterMap.put("userName", userName);
+		inputParameterMap.put("birthdate", birthdate);
+
+
+
+		// すでにログインIDが存在した場合
+		if (request.getAttribute("userList").equals(loginId)) {
+			errorAction(request, response, inputParameterMap);
+			return;
+		}
+
+		// パスワードとパスワード確認の入力内容が異なる場合
+		if (password.equals(passwordConfirm)) {
+			errorAction(request, response, inputParameterMap);
+			return;
+		}
+
+		// 入力項目に未入力がある場合
+		if (!loginId.equals("") || !password.equals("") ||
+				!passwordConfirm.equals("") || !userName.equals("") || !birthdate.equals("")) {
+			errorAction(request, response, inputParameterMap);
+			return;
+		}
+		
+		
+	}
+
+
+	/**
+	 * エラーが発生した際の処理
+	 * @param request
+	 * @param response
+	 * @param inputParameterMap
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	private void errorAction(HttpServletRequest request, HttpServletResponse response,
+			Map<String, String> inputParameterMap) throws ServletException, IOException {
+		request.setAttribute("inputParameter", inputParameterMap);
+		request.setAttribute("errorMessage", "入力された内容は正しくありません");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/userCreate.jsp");
+		dispatcher.forward(request, response);
 	}
 
 }
