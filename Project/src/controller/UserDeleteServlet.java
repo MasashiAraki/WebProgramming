acb8126714d312879119de4f315ee6cfaa94f14a
ArakiMasashi <masa121codbf@gmail.com>
2018-07-02 13:26:40 +0900

    add：一行レコード削除メソッド

--- Project/src/controller/UserDeleteServlet.java
+++ Project/src/controller/UserDeleteServlet.java
@@ -1,11 +1,17 @@
 package controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.UserDao;
+import model.User;
 
 /**
  * Servlet implementation class UserDeleteServlet
@@ -13,7 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/UserDeleteServlet")
 public class UserDeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,16 +32,34 @@ public class UserDeleteServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+    	HttpSession session = request.getSession();
+    	if (session.getAttribute("userInfo") == null) {
+    		response.sendRedirect("LoginServlet");
+    		return;
+    	}
+
+    	request.setCharacterEncoding("UTF-8");
+    	String id = request.getParameter("id");
+
+    	UserDao userDao = new UserDao();
+		User user = userDao.findByUserInfo(id);
+    	request.setAttribute("user", user);
+
+    	RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/userDelete.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		request.setCharacterEncoding("UTF-8");
+		String id = request.getParameter("id");
+
+		UserDao userDao = new UserDao();
+		userDao.DeleteUserInfo(id);
+
+		response.sendRedirect("UserListServlet");
 	}
 
 }
